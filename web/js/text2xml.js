0204557bed9cf9b59167cfa59c7bc5012c359d97
tangzhan_ <tcimpk@gmail.com>
2013-08-14 21:46:20 +0800

    complete web version

--- web/js/text2xml.js
+++ web/js/text2xml.js
@@ -1,4 +1,22 @@
 var Text2Xml = {
+	gettagname: function(l){
+		var tagname = "";
+		var ltrim = $.trim(l);
+		if(ltrim.indexOf(" ") > -1 ){
+			tagname = ltrim.substring(1, ltrim.indexOf(" "));
+		}else{
+			tagname = ltrim.substring(1, ltrim.indexOf(">"));
+		}
+		return tagname
+	},
+	gettaglv: function(l){
+		var lv = 0;
+		for (var i=0; i < l.length; i++) {
+		  if(" "===l[i]){ lv++; }
+		  else{ break; }
+		};
+		return lv;
+	},
 	parseline: function(line){
 		var lineresult = "";
 		//cal whitespace
@@ -50,9 +68,45 @@ var Text2Xml = {
 		  var line = lines[i];
 		  result += Text2Xml.parseline(line);
 		};
+		//去掉最后一个换行
+		result = result.substring(0, result.length-1);
 		//用正则补全标记
 		var closetags = new Array(); //whitespacenum_tagname, e.x. 0_books,1_book  //看contans "/" 就知道这行有没有闭合 //记录last whitespacenum， 如果 cur >= last continue  , cur < last, 补一个关闭标签+"\n"
-		
-		return result;
+		var eachline = result.split("\n");
+		var finalresult = "";
+		for (var j=0; j < eachline.length; j++) {
+		  var l = eachline[j];
+		  //处理同级标签的闭合问题
+		  var lllv = Text2Xml.gettaglv(l);
+		  var lastcloselv = closetags.length>0? closetags[closetags.length-1].split("_")[0] : undefined ;
+		  if( lastcloselv !== undefined && lllv == lastcloselv){
+		  	var close = closetags.pop();
+			var llv = close.split("_")[0];
+			var tag = close.split("_")[1];
+			if(llv > 0){
+				for (var k=0; k < llv; k++) {
+				  finalresult += " ";
+				};
+			}
+			finalresult += (tag + "\n") ;
+		  }
+		  finalresult += (l + "\n");
+		  if(l.indexOf("/")>-1){ continue; }
+		  var ltagname = Text2Xml.gettagname(l);
+		  var llv = Text2Xml.gettaglv(l);
+		  closetags.push(llv+"_</"+ltagname+">");
+		};
+		while(closetags.length>0){
+			var close = closetags.pop();
+			var llv = close.split("_")[0];
+			var tag = close.split("_")[1];
+			if(llv > 0){
+				for (var t=0; t < llv; t++) {
+				  finalresult += " ";
+				};
+			}
+			finalresult += (tag + "\n") ;
+		}
+		return finalresult;
 	}
 }
